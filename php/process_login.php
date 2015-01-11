946f989dbabc24d14405b9a99e9cb880f13b9be1
Mark Vejvoda <mark_vejvoda@hotmail.com>
2015-01-11 15:26:58 -0800

    - added better error checking in login process

--- php/process_login.php
+++ php/process_login.php
@@ -5,10 +5,13 @@
 // ==============================================================
 
 define( 'INCLUSION_PERMITTED', true );
-require_once( 'config.php' );
-require_once( 'functions.php' );
+
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
+
+require_once( 'config.php' );
+require_once( 'functions.php' );
+require_once( 'logging.php' );
 
 sec_session_start(); // Our custom secure way of starting a PHP session.
  
@@ -19,21 +22,34 @@ if (isset($_POST['firehall_id'], $_POST['user_id'], $_POST['p'])) {
 
     $db_connection = null;
     $FIREHALL = findFireHallConfigById($firehall_id, $FIREHALLS);
-    $db_connection = db_connect_firehall($FIREHALL);
-    
-    if (login($FIREHALL,$user_id, $password, $db_connection) == true) {
-        // Login success 
-        header('Location: admin_index.php');
-        //echo 'Login Success!' . PHP_EOL;
-        
-    } 
+    if(isset($FIREHALL)) {
+	    $db_connection = db_connect_firehall($FIREHALL);
+
+	    if(isset($db_connection)) {
+		    if (login($FIREHALL,$user_id, $password, $db_connection) == true) {
+		        // Login success 
+		        header('Location: admin_index.php');
+		        //echo 'Login Success!' . PHP_EOL;
+		    } 
+		    else {
+		        // Login failed 
+		        //header('Location: ../index.php?error=1');
+		    	echo 'Login FAILED.' . PHP_EOL;
+		    }
+	    }
+	    else {
+	    	$log->error("process_login error, no db connection found for firhall id: $firehall_id");
+	    	echo 'Invalid fhdb Request';
+	    }
+    }
     else {
-        // Login failed 
-        //header('Location: ../index.php?error=1');
-    	echo 'Login FAILED.' . PHP_EOL;
+    	$log->error("process_login error, no firehall found for id: $firehall_id");
+    	echo 'Invalid fh Request';
     }
 } 
 else {
-    // The correct POST variables were not sent to this page. 
+    // The correct POST variables were not sent to this page.
+	$log->error("process_login error invalid query params!");
+	
     echo 'Invalid Request';
 }
