3aa577f01b689e916f5a28b9fe099b40ac45d534
sgvfr <dlloyd@sgvfr.com>
2015-02-17 18:10:49 -0800

    Initial commit for new graphs
    - Added combined totals for all callouts
    - filter TESTONLY and TRAINING calltypes from graphs
    - Include Cancelled and Completed calls in graph stats
    - make graphs "pretty", modified graph types and sizes
    - initial testing of ajax redraw for graphs when window is resized,
    without flooding the server with requests.
    - set default back to new UI

--- php/plugins/sms-callout/sgvfr.class.php
+++ php/plugins/sms-callout/sgvfr.class.php
@@ -36,7 +36,7 @@ class SMSCallout_SGVFR_Plugin implements ISMSCalloutPlugin {
 		
 		// First we will send our sms callout to officers
 		$recipients_officers = get_sms_recipients_ldap($callout->getFirehall(),
-				"(&(memberOf=cn=SGVFR-OFFICERS-TEST,ou=Groups,dc=sgvfr,dc=lan)(memberOf=cn=SGVFR-SMSCALLOUT-TEST,ou=Groups,dc=sgvfr,dc=lan))");
+				"(&(memberOf=cn=SGVFR-OFFICERS-TEST_,ou=Groups,dc=sgvfr,dc=lan)(memberOf=cn=SGVFR-SMSCALLOUT-TEST_,ou=Groups,dc=sgvfr,dc=lan))");
 		//echo "Officers found: [$recipients_officers]" . PHP_EOL;
 		
 		if(isset($recipients_officers) && strlen($recipients_officers) > 0) {
@@ -58,7 +58,7 @@ class SMSCallout_SGVFR_Plugin implements ISMSCalloutPlugin {
 						$recipient_list_type, $smsText);
 				
 				if(isset($resultSMS)) {
-					echo $resultSMS;
+					echo $resultSMS . PHP_EOL;
 				}
 			}
 		}
