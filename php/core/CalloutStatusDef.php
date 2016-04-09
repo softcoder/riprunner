d92a1ea7ef39e95812caa7c9c9a04f4d383b56fd
Mark Vejvoda <mark_vejvoda@hotmail.com>
2016-04-09 12:12:14 -0700

    - missed addign new files

---
+++ php/core/CalloutStatusDef.php
@@ -0,0 +1,130 @@
+<?php
+// ==============================================================
+//	Copyright (C) 2016 Mark Vejvoda
+//	Under GNU GPL v3.0
+// ==============================================================
+namespace riprunner;
+
+if ( defined('INCLUSION_PERMITTED') === false ||
+    ( defined('INCLUSION_PERMITTED') === true && INCLUSION_PERMITTED === false ) ) {
+        die( 'This file must not be invoked directly.' );
+    }
+
+if(defined('__RIPRUNNER_ROOT__') === false) {
+    define('__RIPRUNNER_ROOT__', dirname(__FILE__));
+}
+
+require_once __RIPRUNNER_ROOT__.'/config_constants.php';
+
+// Types of status flags
+abstract class StatusFlagType {
+    const STATUS_FLAG_NONE            = 0;
+    const STATUS_FLAG_RESPONDING      = 0x1;
+    const STATUS_FLAG_NOT_RESPONDING  = 0x2;
+    const STATUS_FLAG_CANCELLED       = 0x4;
+    const STATUS_FLAG_COMPLETED       = 0x8;
+    const STATUS_FLAG_STANDBY         = 0x10;
+}
+
+// Types of status flags
+abstract class BehaviourFlagType {
+    const BEHAVIOUR_FLAG_NONE               = 0;
+    const BEHAVIOUR_FLAG_TESTING            = 0x1;
+    const BEHAVIOUR_FLAG_SIGNAL_ALL         = 0x2;
+    const BEHAVIOUR_FLAG_SIGNAL_RESPONDERS  = 0x4;
+    const BEHAVIOUR_FLAG_NON_RESPONDERS     = 0x8;
+    const BEHAVIOUR_FLAG_DEFAULT_RESPONSE   = 0x10;
+}
+
+class CalloutStatusDef {
+
+    private $id;
+    private $name;
+    private $displayName;
+    
+    private $statusFlags;
+    private $behaviourFlags;
+    private $accessFlags;
+      
+    public function __construct($id,$name,$displayName,
+            $statusFlags,$behaviourFlags,$accessFlags, $accessFlagsInclusive) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->displayName = $displayName;
+        $this->statusFlags = $statusFlags;
+        $this->behaviourFlags = $behaviourFlags;
+        $this->accessFlags = $accessFlags;
+        $this->accessFlagsInclusive = $accessFlagsInclusive;
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function getDisplayName() {
+        return $this->displayName;
+    }
+    
+    public function IsResponding() {
+        return ($this->statusFlags != null && ($this->statusFlags & StatusFlagType::STATUS_FLAG_RESPONDING));
+    }
+    public function IsNotResponding() {
+        return ($this->statusFlags != null && ($this->statusFlags & StatusFlagType::STATUS_FLAG_NOT_RESPONDING));
+    }
+    public function IsCancelled() {
+        return ($this->statusFlags != null && ($this->statusFlags & StatusFlagType::STATUS_FLAG_CANCELLED));
+    }
+    public function IsCompleted() {
+        return ($this->statusFlags != null && ($this->statusFlags & StatusFlagType::STATUS_FLAG_COMPLETED));
+    }
+    public function IsStandby() {
+        return ($this->statusFlags != null && ($this->statusFlags & StatusFlagType::STATUS_FLAG_STANDBY));
+    }
+    
+    public function IsTesting() {
+        return ($this->behaviourFlags != null && ($this->behaviourFlags & BehaviourFlagType::BEHAVIOUR_FLAG_TESTING));
+    }
+    public function IsSignalAll() {
+        return ($this->behaviourFlags != null && ($this->behaviourFlags & BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL));
+    }
+    public function IsSignalResponders() {
+        return ($this->behaviourFlags != null && ($this->behaviourFlags & BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS));
+    }
+    public function IsSignalNonResponders() {
+        return ($this->behaviourFlags != null && ($this->behaviourFlags & BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS));
+    }
+    public function IsDefaultResponse() {
+        return ($this->behaviourFlags != null && ($this->behaviourFlags & BehaviourFlagType::BEHAVIOUR_FLAG_DEFAULT_RESPONSE));
+    }
+    
+    public function hasAccess($userAccess) {
+        $validateList = $this->getAccessFlagsValidateList();
+        if($this->accessFlags != null && count($validateList) > 0) {
+            if($userAccess != null) {
+                foreach($validateList as &$access) {
+                    if($this->accessFlags & $access) {
+                        if(!($userAccess & $access)) {
+                            if($this->accessFlagsInclusive) {
+                                return false;
+                            }
+                        }
+                        else if($this->accessFlagsInclusive == false) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    private function getAccessFlagsValidateList() {
+        $validateList = array(USER_ACCESS_ADMIN,USER_ACCESS_SIGNAL_SMS,
+                              USER_ACCESS_CALLOUT_RESPOND_SELF,USER_ACCESS_CALLOUT_RESPOND_SELF,
+                              USER_ACCESS_CALLOUT_RESPOND_OTHERS);
+    }
+}
---
+++ php/core/CalloutStatusType.php
@@ -0,0 +1,169 @@
+<?php
+// ==============================================================
+//	Copyright (C) 2016 Mark Vejvoda
+//	Under GNU GPL v3.0
+// ==============================================================
+namespace riprunner;
+
+require_once 'CalloutStatusDef.php';
+
+abstract class BasicEnum {
+    private static $constCacheArray = NULL;
+
+    private static function getConstants() {
+        if (self::$constCacheArray == NULL) {
+            self::$constCacheArray = array();
+        }
+        $calledClass = get_called_class();
+        if (!array_key_exists($calledClass, self::$constCacheArray)) {
+            $reflect = new \ReflectionClass($calledClass);
+            self::$constCacheArray[$calledClass] = $reflect->getConstants();
+        }
+        return self::$constCacheArray[$calledClass];
+    }
+
+    public static function isValidName($name, $strict = false) {
+        $constants = self::getConstants();
+
+        if ($strict) {
+            return array_key_exists($name, $constants);
+        }
+
+        $keys = array_map('strtolower', array_keys($constants));
+        return in_array(strtolower($name), $keys);
+    }
+
+    public static function isValidValue($value, $strict = true) {
+        $values = array_values(self::getConstants());
+        return in_array($value, $values, $strict);
+    }
+}
+
+// Types of callout responder statuses
+class CalloutStatusType {
+    /*
+    const Paged = 0;
+    const Notified = 1;
+    const Responding = 2;
+    const Cancelled = 3;
+    const NotResponding = 4;
+    const Standby = 5;
+    const Responding_at_hall = 6;
+    const Responding_to_scene = 7;
+    const Responding_at_scene = 8;
+    const Responding_return_hall = 9;
+    const Complete = 10;
+    */
+    
+    static public function getStatusList() {
+        static $statusList = null;
+        if($statusList == null) {
+            $statusList = array(
+                0 => new CalloutStatusDef(0,'Paged','Paged',StatusFlagType::STATUS_FLAG_NONE,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL | 
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS | 
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                    USER_ACCESS_ADMIN,true),
+                1 => new CalloutStatusDef(1,'Notified','Notified',StatusFlagType::STATUS_FLAG_NONE,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                    USER_ACCESS_ADMIN,true),
+                2 => new CalloutStatusDef(2,'Responding','Respond to hall',StatusFlagType::STATUS_FLAG_RESPONDING,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_DEFAULT_RESPONSE,
+                    USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+                3 => new CalloutStatusDef(3,'Cancelled','Cancelled',StatusFlagType::STATUS_FLAG_CANCELLED,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                    USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+                4 => new CalloutStatusDef(4,'NotResponding','Not Responding',StatusFlagType::STATUS_FLAG_NOT_RESPONDING,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NONE,
+                    USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+                5 => new CalloutStatusDef(5,'Standby','Standby',StatusFlagType::STATUS_FLAG_STANDBY,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                    USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+                6 => new CalloutStatusDef(6,'Responding_at_hall','Respond at hall',StatusFlagType::STATUS_FLAG_RESPONDING,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                    USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+                7 => new CalloutStatusDef(7,'Responding_to_scene','Respond to scene',StatusFlagType::STATUS_FLAG_RESPONDING,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                    USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+                8 => new CalloutStatusDef(8,'Responding_at_scene','Respond at scene',StatusFlagType::STATUS_FLAG_RESPONDING,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                    USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+                9 => new CalloutStatusDef(9,'Responding_return_hall','Return to hall',StatusFlagType::STATUS_FLAG_RESPONDING,
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                    BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                    USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+                10 => new CalloutStatusDef(10,'Complete','Complete',StatusFlagType::STATUS_FLAG_COMPLETED,
+                        BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_ALL |
+                        BehaviourFlagType::BEHAVIOUR_FLAG_SIGNAL_RESPONDERS |
+                        BehaviourFlagType::BEHAVIOUR_FLAG_NON_RESPONDERS,
+                        USER_ACCESS_ADMIN | USER_ACCESS_CALLOUT_RESPOND_SELF | USER_ACCESS_CALLOUT_RESPOND_OTHERS,false),
+            );
+        }
+        return $statusList;
+    }
+    static public function getStatusById($id) {
+        $statusList = self::getStatusList();
+        return $statusList[$id];
+    }
+    static public function getStatusByName($name,$strict=false) {
+        $statuses = self::getStatusList();
+        foreach($statuses as &$status) {
+            if ($strict) {
+                if($status->getName() == $name) {
+                    return $status;
+                }
+            }
+            else if(strtolower($status->getName()) == strtolower($name)) {
+                return $status;
+            }
+        }
+        return null;;
+        
+    }
+    static public function isValidValue($id) {
+        $statuses = self::getStatusList();
+        return array_key_exists($id, $statuses);
+    }
+    static public function isValidName($name, $strict=false) {
+        $statuses = self::getStatusList();
+        foreach($statuses as &$status) {
+            if ($strict) {
+                if($status->getName() == $name) {
+                    return true;
+                }
+            }
+            else if(strtolower($status->getName()) == strtolower($name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    static public function Paged() { return self::getStatusById(0); }
+    static public function Notified() { return self::getStatusById(1); }
+    static public function Responding() { return self::getStatusById(2); }
+    static public function Cancelled() { return self::getStatusById(3); }
+    static public function NotResponding() { return self::getStatusById(4); }
+    static public function Standby() { return self::getStatusById(5); }
+    static public function Responding_at_hall() { return self::getStatusById(6); }
+    static public function Responding_to_scene() { return self::getStatusById(7); }
+    static public function Responding_at_scene() { return self::getStatusById(8); }
+    static public function Responding_return_hall() { return self::getStatusById(9); }
+    static public function Complete() { return self::getStatusById(10); }
+}
