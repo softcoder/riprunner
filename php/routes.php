aea1c57533cb2bbc0d3a7462b093884bbd25a38c
Mark Vejvoda <mark_vejvoda@hotmail.com>
2016-07-23 22:07:53 -0700

    - default root url now goes to login

--- php/routes.php
+++ php/routes.php
@@ -15,9 +15,50 @@ if(defined('INCLUSION_PERMITTED') === false) {
     define( 'INCLUSION_PERMITTED', true);
 }
 
-require_once 'config.php';
+//require_once 'config.php';
+try {
+    if (!file_exists('config.php' )) {
+        throw new Exception ('Config script does not exist!');
+    }
+    else {
+        require_once 'config.php';
+    }
+}
+catch(Exception $e) {
+    echo '<!DOCTYPE html>'.PHP_EOL.
+    '<html>'.PHP_EOL.
+    '<head>'.PHP_EOL.
+    '<meta charset="UTF-8">'.PHP_EOL.
+    '<title>Installation Page for: '.PRODUCT_NAME.'</title>'.PHP_EOL.
+    '<link rel="stylesheet" href="styles/main.css" />'.PHP_EOL.
+    '</head>'.PHP_EOL.
+    '<body>'.PHP_EOL.
+    '<p style="font-size:40px; color: white">'.
+    PRODUCT_NAME.' v'.CURRENT_VERSION.' - '.PRODUCT_URL.'<br>'.
+    '<hr></p>'.PHP_EOL.
+    '<p style="font-size:35px; color: red">'.PHP_EOL.
+    'Error detected, message : ' . $e->getMessage().', '.'Code : ' . $e->getCode().
+    '<br><span style="font-size:35px; color: yellow">Please create a config.php script.</span>'.PHP_EOL.
+    '<br><b><a target="_blank" href="config-builder.php">Click here to generate a config.php</a>
+        <br>Refresh this page once the config.php has been saved in the riprunner php folder of the server.</b>'.PHP_EOL.
+        '</p><hr>'.PHP_EOL.
+        '</body>'.PHP_EOL.
+        '</html>';
+    return;
+}
+
 require_once __RIPRUNNER_ROOT__ . '/functions.php';
 require_once __RIPRUNNER_ROOT__ . '/third-party/flight/Flight.php' ;
+
+\Flight::route('GET|POST /', function () {
+    global $FIREHALLS;
+    //$query = array();
+    //parse_str($params, $query);
+
+    $root_url = getFirehallRootURLFromRequest(\Flight::request()->url, $FIREHALLS);
+    //\Flight::redirect($root_url .'controllers/login-controller.php?' . $params);
+    \Flight::redirect($root_url .'controllers/login-controller.php');
+});
 
 \Flight::route('GET|POST /login|/logon(/@params)', function ($params) {
 	global $FIREHALLS;
