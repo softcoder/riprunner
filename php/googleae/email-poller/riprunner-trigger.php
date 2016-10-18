d624feb9ed79f801e3812f08f4c679653f1a868e
Ron Ammundsen <ronam@users.noreply.github.com>
2016-10-18 08:16:57 -0700

    Update riprunner-trigger.php

--- php/googleae/email-poller/riprunner-trigger.php
+++ php/googleae/email-poller/riprunner-trigger.php
@@ -1,8 +1,11 @@
 <?php
 
-// Point to the Main Email trigger URL which will poll for all Firehalls
-// Example replace: http://soft-haus.com/svvfd/riprunner/ with the root of you installation
-$result = file_get_contents('http://soft-haus.com/svvfd/riprunner/email_trigger_check.php?'.mt_rand());
+// Load the Main Email trigger URL which will poll for all Firehalls
+
+define('INCLUSION_PERMITTED', true);
+require_once 'config.php';
+
+$result = file_get_contents( EMAIL_TRIGGER_CHECK_BASE_URL . 'email_trigger_check.php?' . mt_rand() );
 
 echo 'Got Results:' . PHP_EOL;
 echo $result;
