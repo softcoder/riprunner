ac3363c5b609a24acba5db6399776f55a2d38a5c
Mark Vejvoda <mark_vejvoda@hotmail.com>
2018-01-01 14:48:11 -0800

    - fix broken test

--- php/config-test.php
+++ php/config-test.php
@@ -186,7 +186,7 @@ $GOOGLE_MAP_CITY_LOOKUP = array(
 	
 	// ----------------------------------------------------------------------
 	// Website and Location Settings
-	define( 'DEFAULT_WEBSITE_GOOGLE_MAP_API_KEY', 'X' );
+	define( 'DEFAULT_WEBSITE_GOOGLE_MAP_API_KEY', 'AIzaSyCy8uwfS_I2Iruqm1h8vczV8L_llgDirPk' );
 
 	// ----------------------------------------------------------------------
 	// Email parser lookup patterns for email triggers
