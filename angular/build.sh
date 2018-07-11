4341fd3ca31a117015fda456d68149982f9c07f2
Mark Vejvoda <mark_vejvoda@hotmail.com>
2018-07-11 16:25:52 -0700

    - fixed compiling in angular 6+ with --aot

--- angular/build.sh
+++ angular/build.sh
@@ -3,4 +3,4 @@ echo 'Building Rip Runner Angular Components...'
 # Angular 5
 # ng build --base-href=/~softcoder/svvfd1/php/ngui/ --output-path=../php/ngui/ --aot
 # Angular 6+
-ng build --base-href=/~softcoder/svvfd1/php/ngui/ --output-path=../php/ngui/ --configuration=dev
+ng build --base-href=/~softcoder/svvfd1/php/ngui/ --output-path=../php/ngui/ --configuration=dev --aot
