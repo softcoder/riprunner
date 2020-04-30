d2cfc95ed04b5d056091ab6517a1288b5711641e
sgvfr <dlloyd@sgvfr.com>
2020-04-30 08:15:19 -0700

    merge from softcoder and css changes
    merge softcoder (4aff5e0 - added config builder unit test)
    
    more css work for global tables
     - minor line height change in body
     - removed many old and unused styles from main

---
+++ php/tests/config/configBuilderTest.php
@@ -0,0 +1,51 @@
+<?php
+// ==============================================================
+//	Copyright (C) 2020 Mark Vejvoda
+//	Under GNU GPL v3.0
+// ==============================================================
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+if(defined('INCLUSION_PERMITTED') === false) {
+    define( 'INCLUSION_PERMITTED', true);
+}
+
+require_once dirname(dirname(__FILE__)).'/baseDBFixture.php';
+$rootPath = dirname(dirname(dirname(__FILE__)));
+
+class ConfigBuilderTest extends BaseDBFixture {
+	
+    protected function setUp() {
+        // Add special fixture setup here
+        parent::setUp();
+    }
+    
+    protected function tearDown() {
+        // Add special fixture teardown here
+        parent::tearDown();
+    }
+
+	public function testConfigBuilderLoad_ConfigInValid()  {
+        global $rootPath;
+        try {
+            $rename_status1 = rename($rootPath.'/config.php', $rootPath.'/config-invalid.php');
+            ob_start();
+            require_once $rootPath.'/config-builder.php';
+            $unit_test_output = ob_get_clean();
+            $config_exists = file_exists('config.php');
+        }
+        finally {
+            $rename_status2 = rename($rootPath.'/config-invalid.php', $rootPath.'/config.php');
+        }
+
+        //echo "****** #1 TESTING ***** RENAME: [$rename_status] " . $unit_test_output . PHP_EOL;
+        $this->assertEquals(true, $rename_status1);
+        $this->assertEquals(true, $rename_status2);
+        $this->assertEquals(false, $config_exists);
+        $this->assertContains('<html>', $unit_test_output);
+        $this->assertContains('</html>', $unit_test_output);
+        $this->assertContains('Rip Runner Configuration Generator', $unit_test_output);
+        $this->assertContains('Generate Configuration', $unit_test_output);
+	}
+	
+}
